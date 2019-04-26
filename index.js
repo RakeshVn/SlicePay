b95e70979d9f660d75d271dbb0d2746f91b4326f
sys3003 <rakesh.v@way2online.co.in>
2019-04-26 18:34:00 +0530

    npm init and node_modules installed

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 // config should be imported before importing any other file
-const config = require('./config/config');
+// const config = require('./config/config');
 const app = require('./config/express');
-require('./config/mongoose');
+// require('./config/mongoose');
 
-// module.parent check is required to support mocha watch
-// src: https://github.com/mochajs/mocha/issues/1912
-if (!module.parent) {
-  app.listen(config.port, () => {
+app.listen(config.port, () => {
     console.info(`server started on port ${config.port} (${config.env})`);
-  });
-}
+});
 
 module.exports = app;
